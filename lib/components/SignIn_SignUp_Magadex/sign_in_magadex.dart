b0c68befb040991bb465f6d82d7b430aea9aeb23
lazyty <133588082+lazyty@users.noreply.github.com>
2024-07-02 17:02:52 +0700

    components change

---
+++ lib/components/SignIn_SignUp_Magadex/sign_in_magadex.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:oms/API/authencation.dart';
+import 'package:oms/components/api_variables.dart';
+import 'package:oms/config/app_config.dart';
+
+class LoginFormDialog extends StatefulWidget {
+  @override
+  const LoginFormDialog({Key? key}) : super(key: key);
+  State<LoginFormDialog> createState() => _LoginFormDialogState();
+}
+
+class _LoginFormDialogState extends State<LoginFormDialog> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<bool> CheckAccountMangadex(String user, String password) async {
+    if (user.isEmpty || password.isEmpty) {
+      print('Please fill all fields');
+      return false;
+    }
+    QuerySnapshot querySnapshot =
+        await firestore.collection('mangadex_account').get();
+    for (var doc in querySnapshot.docs) {
+      if (doc.id == user && doc['password'] == password) {
+        String clientId = doc['clientId'];
+        String secretID = doc['secretId'];
+        String? response =
+            await getResfreshingToken(user, password, clientId, secretID);
+        if (response != null) {
+          apiVariables.clientId = clientId;
+          apiVariables.secretId = secretID;
+          apiVariables.refreshToken = response;
+          apiVariables.username = user;
+          apiVariables.password = password;
+          apiVariables.isLogin = true;
+          if (apiVariables.refreshToken != null) {
+            return true;
+          } else {
+            print("failllll");
+            return false;
+          }
+        }
+      } else {
+        print("fail dang ki");
+        //in gi do
+      }
+    }
+    return false;
+  }
+
+  // dung de xu ly tinh nang
+  //  Future<bool> CheckAccountMangadex(String user,String password) async {
+  //       user = 'Thanh8806';
+  //       password = 'thanh080804@gmail.com';
+  //       String clientId = 'personal-client-b899bbf6-0f42-4136-96a2-74fbeb8d9176-dd343205';
+  //       String secretID= 'R4Lopt85Qgv7keEEGLMdK7dRCCW5kCCH';
+  //       String? response = await getResfreshingToken(user, password, clientId, secretID);
+  //       if(response != null){
+  //         apiVariables.clientId = clientId;
+  //         apiVariables.secretId = secretID;
+  //         apiVariables.refreshToken = response;
+  //         apiVariables.username = user;
+  //         apiVariables.password = password;
+  //         apiVariables.isLogin = true;
+  //         if(apiVariables.refreshToken != null){
+  //           return true;
+  //         }
+  //         else{
+  //           return false;
+  //         }
+  //         }
+  //       else{
+  //         return false;
+  //       }
+  // }
+
+  bool _isHidden = true;
+  void _toggleVisibility() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 40,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Sign In to Mangadex Account',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Please enter your username and password of your Mangadex account to login.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              TextField(
+                obscureText: _isHidden,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                    onPressed: _toggleVisibility,
+                    icon: _isHidden
+                        ? Icon(Icons.visibility_off)
+                        : Icon(Icons.visibility),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Handle register action here
+                  Navigator.pushNamed(context, 'signUpMangadex');
+                },
+                child: Text(
+                  'Register here',
+                  style: TextStyle(color: Colors.deepOrange),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle Cancel action
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      // Handle Login action
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
+                      // Implement login logic here
+                      bool check =
+                          await CheckAccountMangadex(username, password);
+                      if (check == true) {
+                        Navigator.pop(context, true);
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Login failed'),
+                              content: Text(
+                                  'Please check your username and password again'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    _usernameController.text = '';
+                                    _passwordController.text = '';
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Text('Login'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/SignIn_SignUp_Magadex/sign_up_mangadex.dart
@@ -0,0 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:oms/API/authencation.dart';
+import 'package:oms/components/api_variables.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class RegisterFormDialog extends StatefulWidget {
+  @override
+  _RegisterFormDialogState createState() => _RegisterFormDialogState();
+}
+
+class _RegisterFormDialogState extends State<RegisterFormDialog> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _clientIdController = TextEditingController();
+  final TextEditingController _secretIdController = TextEditingController();
+  
+  Future<void> IsEmptyAccount(String username, String password, String clientId, String secretId) async {
+    if (username.isEmpty || password.isEmpty || clientId.isEmpty || secretId.isEmpty) {
+      return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Register failed'),
+            content: Text('Please fill in all fields'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  _usernameController.text = '';
+                  _passwordController.text = '';
+                  _clientIdController.text = '';
+                  _secretIdController.text = '';
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } 
+    String? response = await getResfreshingToken(username, password, clientId, secretId);
+    if(response != null){
+      ApiVariables apiVariables = ApiVariables(
+        username: username,
+        password: password,
+        clientId: clientId,
+        secretId: secretId,
+        isLogin: true,
+        refreshToken: response, 
+      );
+      FetchDataToFireStore(apiVariables: apiVariables);
+      Navigator.of(context).pop();
+      Navigator.of(context).pop();  
+    } else {
+      print('Register failed');
+    }
+  }
+
+  Widget Decrepsion() {
+    return RichText(
+      text:TextSpan(
+        text: "You can get your Client ID and Secret ID by registering an account on Mangadex website. For more information, please visit",
+        style: TextStyle(color: Colors.black),
+        children: [
+          TextSpan(
+            text: "\nMangadex registration link: ",
+          ),
+          TextSpan(
+            text: "https://mangadex.org/",
+            style: TextStyle(decoration: TextDecoration.underline,color: Colors.blue),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                 launchUrl(Uri.parse("https://mangadex.org/"));
+              },
+          ),
+          TextSpan(
+            text: "\nApiclient registration link: ",
+          ),
+          TextSpan(
+            text: "https://mangadex.org/",
+            style: TextStyle(decoration: TextDecoration.underline,color: Colors.blue),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                 launchUrl(Uri.parse("https://mangadex.org/settings"));
+              },
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  bool _isHidden = true;
+  void _toggleVisibility() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 50,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Decrepsion(),
+            SizedBox(height: 5),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                suffixIcon: IconButton(
+                  onPressed: _toggleVisibility,
+                  icon: _isHidden
+                      ? Icon(Icons.visibility_off)
+                      : Icon(Icons.visibility),
+                ),
+                ),
+              obscureText: _isHidden,
+            ),
+            TextField(
+              controller: _clientIdController,
+              decoration: InputDecoration(labelText: 'Client ID'),
+            ),
+            TextField(
+              controller: _secretIdController,
+              decoration: InputDecoration(labelText: 'Secret ID'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle Cancel action
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    // Handle Register action
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+                    String clientId = _clientIdController.text;
+                    String secretId = _secretIdController.text;
+                    // Implement registration logic here
+                    await IsEmptyAccount(username, password, clientId, secretId);
+                  },
+                  child: Text('Register') ,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+    );
+  }
+}
+
+void FetchDataToFireStore({required ApiVariables apiVariables}) {
+  CollectionReference account = FirebaseFirestore.instance.collection('mangadex_account');
+  account 
+  .doc(apiVariables.username)
+  .set(
+    {
+      'password': apiVariables.password,
+      'clientId': apiVariables.clientId,
+      'secretId': apiVariables.secretId,
+    })
+  .then((value) => print('Account added'))
+  .catchError((error) => print('Failed to add account: $error'));
+}
